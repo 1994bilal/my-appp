6a9c7bdec39cdba214b93387d0303da03e880283
1994bilal <bilalfiaz802@gmail.com>
2024-09-12 20:42:22 +0500

    update

--- src/app/Window/page.tsx
+++ src/app/Window/page.tsx
@@ -191,7 +191,7 @@ md:grid grid-cols-3 py-10 gap-1'>
 <div className='text-center my-20 -rotate-0'>
         
          
-         <div className='my-10'><h1 className='bg-purple-600 inline rounded-[20px] text-white text-2xl text-center py-8 px-20' >Sample Window Sticker</h1></div>
+         <div className='my-10'><h1 className='bg-purple-600 inline rounded-[20px] text-white md:text-2xl text-xl text-center py-4 px-10' >Sample Window Sticker</h1></div>
          <div className='mt-[-10px] mx-20'><h3 className='text-[16px]
          bg-blue-300 inline rounded-[20px] text-white md:text-[22px] font-extralight md:py-5 md:px-2 px-3 md:ml-40 my-8'>View Some of Our Window Stickers</h3></div>
         </div>
